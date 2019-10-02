5ae41e4515941b61411716ba86e1a0d917629705
Aleksandr Falichev <6064233@gmail.com>
2019-10-02 11:55:32 +0800

    рефоктор ФТ. Вынос части функционал в базовый класс

--- functional_tests/test_simple_creation_bell.py
+++ functional_tests/test_simple_creation_bell.py
@@ -1,62 +1,15 @@
 import re
-import time
 
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 
-MAX_WAIT = 20
+from functional_tests.base import FunctionalTest
 
 
-def wait(fn):
-    """декоратор для явного ожидания работы функции"""
+class NewVisitorTest(FunctionalTest):
+    """Функциональный тест для нового посетителя"""
 
-    def modified_fn(*args, **kwargs):
-        start_time = time.time()
-        while True:
-            try:
-                # поппытка выполнить функцию
-                return fn(*args, **kwargs)
-
-            # если ошибка сравнения или ошибка браузера
-            except (AssertionError, WebDriverException) as e:
-
-                # если превышен таймаут, то ошибка
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-
-                # если таймаут не вышел, то попробуем выполнить еще разок
-                time.sleep(0.5)
-
-    return modified_fn
-
-
-class FunctionalTest(StaticLiveServerTestCase):
-    """Функциональный тест"""
-
-    def setUp(self) -> None:
-        self.browser = webdriver.Chrome("/usr/bin/chromedriver")
-
-    def tearDown(self) -> None:
-        self.browser.quit()
-
-    @wait
-    def wait_for_ring(self):
-        """ожидать когда зазвенит колокольчик"""
-
-        self.browser.find_element_by_css_selector(".bell .ring")
-        # не должно поднять исключение
-
-    @wait
-    def wait_for_stop_ring(self):
-        """ожидать когда зазвенит колокольчик"""
-
-        self.browser.find_element_by_css_selector(".bell .stop-ring")
-        # не должно поднять исключение
-
-    def test_new_bell(self):
-        """создать новый колокольчик"""
+    def test_can_create_new_bell(self):
+        """тест: создать новый колокольчик"""
 
         # Иван заходит на сайт
         self.browser.get(self.live_server_url)
