6a9165127138f4b78b201af7a2a34b5f5bd693e0
Aleksandr Falichev <6064233@gmail.com>
2019-10-02 12:12:34 +0800

    ФТ проверки стилей

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,33 @@
+from selenium.webdriver.common.keys import Keys
+
+from functional_tests.base import FunctionalTest
+
+MAX_WAIT = 10
+
+
+class LayoutAndStylingTest(FunctionalTest):
+    """тест макета и стилевого оформления"""
+
+    def test_layout_and_styling(self):
+        """тест макета и стиля"""
+
+        # Иван открывает домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Он замечает что поле ввода аккуратно центрировано
+        inputbox = self.get_bell_title_inputbox()
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # он создает новый колокольчик и видит, что там тоже поле центрировано
+        self.add_new_bell('Сообщение в чате')
+        bell_title = self.browser.find_element_by_css_selector("h1")
+        self.assertAlmostEqual(
+            bell_title.location['x'] + bell_title.size['width'] / 2,
+            512,
+            delta=10
+        )
