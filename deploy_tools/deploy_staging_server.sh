b00cc7f12bc58216157987c66313a052c43fc126
Aleksandr Falichev <6064233@gmail.com>
2019-10-09 22:08:29 +0800

    Изменен адрес сервер в скрипте запуска деплоя

--- deploy_tools/deploy_staging_server.sh
+++ deploy_tools/deploy_staging_server.sh
@@ -1,5 +1,6 @@
 SERVER=62.109.12.113
+STAGING_SERVER=$SERVER:83
 cd deploy_tools
 fab deploy:host=root@$SERVER
 cd ..
-#STAGING_SERVER=$SERVER ../venv/bin/python manage.py test functional_tests
+STAGING_SERVER=$STAGING_SERVER venv/bin/python manage.py test functional_tests
